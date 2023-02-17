5526d07a6f9cfc9db7583acfb209f72b905a941a
Ojogunemmy <ojogunemmy@gmail.com>
2023-02-17 07:04:39 +0100

    On branch master
    Initial commit
    	new file:   about.html
    	new file:   img/an1.jpeg
    	new file:   img/an2.jpeg
    	new file:   img/an3.jpeg
    	new file:   img/an4.jpeg
    	new file:   img/external-content.duckduckgo.com.jpeg
    	new file:   img/logo.jpeg
    	new file:   img/show1.jpeg
    	new file:   img/show2.jpeg
    	new file:   img/show3.jpeg
    	new file:   img/show4.jpeg
    	new file:   img/view1.jpeg
    	new file:   img/view2.jpeg
        new file:   img/view3.jpeg
    	new file:   img/view4.jpeg
    	new file:   img/view5.jpeg
    	new file:   img/view6.jpeg
    	new file:   img/view7.jpeg
    	new file:   img/view8.jpeg
    	new file:   img/view9.jpeg
    	new file:   index.html
    	new file:   login.html
    	new file:   signup.html
    	new file:   static/index.js
    	new file:   static/style.css
    	new file:   videos.html
    	new file:   views.html

---
+++ static/index.js
@@ -0,0 +1,23 @@
+function userSignUp()
+{ 
+    let user = document.getElementById('user').value
+    let email = document.getElementById('email').value
+    let pass = document.getElementById('pass').value
+    let comf_Pass = document.getElementById('comfPass').value
+   
+
+    if(user!=""&&email!=""&&pass!=""&&comf_Pass!=""){
+   
+     alert(user+" Successfully added..")
+    }
+
+
+ 
+ 
+}
+
+function data(){
+    let comf_Pass = document.getElementById('comfPass').value
+
+
+}
